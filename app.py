0f8fff4153961e776622ee05583ab26111d3df8e
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-09 11:35:24 +0200

    Create generator and print_generator_values function

---
+++ app.py
@@ -0,0 +1,20 @@
+"""Generator which stores n values provided by user
+Values calculated based on equation provided 'num_next = (num_previous * 3) // 5'
+num_previous chosen randomly"""
+def generator(user_input):
+    num_previous = 1000
+    """Iterating as many times as user provided"""
+    for _ in range(user_input):
+        num_next = (num_previous * 3) // 5.
+        num_previous = num_next
+        """Stores calculated value"""
+        yield num_next
+
+
+"""Iterates through generator object and prints the value as many times as user provides"""
+def print_generator_values(user_input):
+    values = generator(user_input)
+    for value in values:
+        print(value)
+
+print_generator_values(5)
